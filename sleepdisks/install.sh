2dc41c1ef3e8ebb7fbf530f9eddf7845a6e2071a
AuxXxilium <info@visionz.media>
2022-11-25 19:34:35 +0100

    addons: remove unused
    Signed-off-by: AuxXxilium <info@visionz.media>

--- sleepdisks/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ash
-
-if [ "${1}" = "late" ]; then
-  echo "Installing Patch for Disksleep"
-  tar -zxvf /addons/sleepdisks.tgz -C /tmpRoot/
-  chmod 755 /tmpRoot/usr/bin/sleepdisks
-  chmod 644 /tmpRoot/usr/lib/systemd/system/sleepdisks.service
-  ln -sf /usr/lib/systemd/system/sleepdisks.service /tmpRoot/etc/systemd/system/multi-user.target.wants/sleepdisks.service
-fi
