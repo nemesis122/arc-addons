d06fcb9048636af22f1109ad1a3ef1b85df31dee
AuxXxilium <info@auxxxilium.tech>
2024-03-25 18:02:09 +0100

    photosfacepatch: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- photosfacepatch/install.sh
+++ photosfacepatch/install.sh
@@ -4,26 +4,45 @@ if [ "${1}" = "late" ]; then
   echo "Installing addon photosfacepatch - ${1}"
   mkdir -p "/tmpRoot/usr/arc/addons/"
   cp -vf "${0}" "/tmpRoot/usr/arc/addons/"
-  
-  echo "Creating service to exec photosfacepatch"
-  cp -vf /usr/bin/photosfacepatch.sh /tmpRoot/usr/bin/photosfacepatch.sh
-  cp -vf /usr/bin/PatchELFSharp /tmpRoot/usr/bin/PatchELFSharp
-  
-  DEST="/tmpRoot/usr/lib/systemd/system/photosfacepatch.service"
-  echo "[Unit]"                                                                >${DEST}
-  echo "Description=Enable photosfacepatch"                                   >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Service]"                                                            >>${DEST}
-  echo "Type=oneshot"                                                         >>${DEST}
-  echo "RemainAfterExit=yes"                                                  >>${DEST}
-  echo "ExecStart=/usr/bin/photosfacepatch.sh"                                >>${DEST}
-  echo                                                                        >>${DEST}
-  echo "[Install]"                                                            >>${DEST}
-  echo "WantedBy=multi-user.target"                                           >>${DEST}
 
-  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/photosfacepatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/photosfacepatch.service
+  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
+  if [ -f "${SO_FILE}" ]; then
+    if [ ! -f "${SO_FILE}.bak" ]; then
+      echo "Backup ${SO_FILE}"
+      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
+    fi
+    echo "Patching libsynosdk.so.7"
+    PatchELFSharp "${SO_FILE}" "SYNOFSIsRemoteFS" "B8 00 00 00 00 C3"
+  else
+    echo "libsynosdk.so.7 not found"
+  fi
+  SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
+  if [ -f "${SO_FILE}" ]; then
+    if [ ! -f "${SO_FILE}.bak" ]; then
+      echo "Backup ${SO_FILE}"
+      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
+    fi
+    echo "Patching ${${SO_FILE}}"
+    # support face and concept
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform20IsSupportedIENetworkEv" "B8 00 00 00 00 C3"
+    # force to support concept
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform18IsSupportedConceptEv" "B8 01 00 00 00 C3"
+    # force no Gpu
+    PatchELFSharp "${SO_FILE}" "_ZN9synophoto6plugin8platform23IsSupportedIENetworkGpuEv" "B8 00 00 00 00 C3"
+  else
+    echo "${SO_FILE} not found"
+  fi
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon photosfacepatch - ${1}"
-  #TODO: Add uninstall code here
+
+  SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
+  if [ -f "${SO_FILE}.bak" ]; then
+    echo "Restore ${SO_FILE}"
+    mv -f "${SO_FILE}.bak" "${SO_FILE}"
+  fi
+  SO_FILE="/tmpRoot/var/packages/SynologyPhotos/target/usr/lib/libsynophoto-plugin-platform.so"
+  if [ -f "${SO_FILE}.bak" ]; then
+    echo "Restore ${SO_FILE}"
+    mv -f "${SO_FILE}.bak" "${SO_FILE}"
+  fi
 fi
