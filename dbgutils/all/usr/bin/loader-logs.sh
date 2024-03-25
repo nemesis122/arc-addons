8f4d66d1245d479abd4d884fc7bb4048ded4e455
AuxXxilium <info@auxxxilium.tech>
2024-03-25 20:20:33 +0100

    dbgutils: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- dbgutils/all/usr/bin/loader-logs.sh
+++ dbgutils/all/usr/bin/loader-logs.sh
@@ -8,29 +8,38 @@
 
 [ -z "${1}" ] && echo "Usage: ${0} {early|jrExit|rcExit|late|dsm}" && exit 1
 
-LOADER_DISK_PART1="$(blkid -L ARC1 | cut -d':' -f1)"
+LOADER_DISK_PART1="$(blkid -L ARC1)"
 [ -z "${LOADER_DISK_PART1}" -a -b "/dev/synoboot1" ] && LOADER_DISK_PART1="/dev/synoboot1"
 [ -z "${LOADER_DISK_PART1}" ] && echo "Boot disk not found" && exit 1
 
 modprobe vfat
 echo 1 >/proc/sys/kernel/syno_install_flag
-mkdir -p "/mnt/p1"
 
-mount "${LOADER_DISK_PART1}" "/mnt/p1"
-rm -rf "/mnt/p1/logs/${1}"
-mkdir -p "/mnt/p1/logs/${1}"
-cp -vfR "/var/log/"* "/mnt/p1/logs/${1}"
-dmesg >"/mnt/p1/logs/${1}/dmesg.log"
-lsmod >"/mnt/p1/logs/${1}/lsmod.log"
-lsusb >"/mnt/p1/logs/${1}/lsusb.log"
-lspci -Qnnk >"/mnt/p1/logs/${1}/lspci.log" || true
-sysctl -a >"/mnt/p1/logs/${1}/sysctl.log" || true
-ls -l /dev/ >"/mnt/p1/logs/${1}/disk-dev.log" || true
-ls -l /dev/disk/by-id/ >"/mnt/p1/logs/${1}/disk-by-id.log" || true
-ls -l /sys/class/scsi_host >"/mnt/p1/logs/${1}/disk-scsi_host.log" || true
-ls -l /sys/class/net/*/device/driver >"/mnt/p1/logs/${1}/net-driver.log" || true
-cat /sys/block/s*/device/syno_block_info >"/mnt/p1/logs/${1}/disk-syno_block_info.log" || true
-[ -f "/addons/addons.sh" ] && cp -f "/addons/addons.sh" "/mnt/p1/logs/${1}/addons.sh" || true
-[ -f "/addons/model.dts" ] && cp -f "/addons/model.dts" "/mnt/p1/logs/${1}/model.dts" || true
+WORK_PATH="/mnt/p1"
+mkdir -p "${WORK_PATH}"
+mount "${LOADER_DISK_PART1}" "${WORK_PATH}"
 
-umount "/mnt/p1"
+DEST_PATH="${WORK_PATH}/logs/${1}"
+rm -rf "${DEST_PATH}"
+mkdir -p "${DEST_PATH}"
+
+dmesg >"${DEST_PATH}/dmesg.log"
+lsmod >"${DEST_PATH}/lsmod.log"
+lsusb >"${DEST_PATH}/lsusb.log"
+lspci -Qnnk >"${DEST_PATH}/lspci.log" || true
+sysctl -a >"${DEST_PATH}/sysctl.log" || true
+ls -l /dev/ >"${DEST_PATH}/dev.log" || true
+
+ls -l /sys/class/block >"${DEST_PATH}/disk-block.log" || true
+ls -l /sys/class/scsi_host >"${DEST_PATH}/disk-scsi_host.log" || true
+ls -l /sys/class/net/*/device/driver >"${DEST_PATH}/net-driver.log" || true
+cat /sys/block/*/device/syno_block_info >"${DEST_PATH}/disk-syno_block_info.log" || true
+
+[ -f "/addons/addons.sh" ] && cp -f "/addons/addons.sh" "${DEST_PATH}/addons.sh" || true
+[ -f "/addons/model.dts" ] && cp -f "/addons/model.dts" "${DEST_PATH}/model.dts" || true
+
+[ -f "/var/log/messages" ] && cp -f "/var/log/messages" "${DEST_PATH}/messages" || true
+[ -f "/var/log/linuxrc.syno.log" ] && cp -f "/var/log/linuxrc.syno.log" "${DEST_PATH}/linuxrc.syno.log" || true
+[ -f "/tmp/installer_sh.log" ] && cp -f "/tmp/installer_sh.log" "${DEST_PATH}/installer_sh.log" || true
+
+umount "${WORK_PATH}"
