f6ab0192ea207116895454eab0517f1b3df8f292
AuxXxilium <info@auxxxilium.tech>
2024-04-01 13:08:23 +0200

    notify: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- notify/all/usr/bin/notify.sh
+++ notify/all/usr/bin/notify.sh
@@ -25,7 +25,19 @@ else
 
   # NOTIFICATION="Arc Notification"
   # synodsmnotify -e false -b false "@administrators" "arc_notify" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\"}"
-  NOTIFICATION="Arc Notification"
-  SUBJECT="Welcome to <a href=\\\"https://github.com/AuxXxilium\\\" target=blank>Arc</a>!"
+  NOTIFICATION="Arc Loader Notification"
+  SUBJECT="$(cat <<EOF
+<p>Welcome to <a href="https://github.com/AuxXxilium" target="_blank">Arc</a> Loader!</p>
+<p></p>
+<p>You have successfully installed Synology DSM on your System.</p>
+<p></p>
+<p>If you encounter any issues, please read <a href="https://github.com/AuxXxilium/AuxXxilium/wiki" target="_blank">Wiki</a> first.</p>
+<p>Feel free to join our <a href="https://discord.gg/auxxxilium" target="_blank">Discord</a>.</p>
+<p></p>
+<p>To reboot to Config Mode, you can enable SSH and do: loader-reboot.sh config</p>
+<p>Open VM Tools for ESXI can be found at <a href="https://github.com/AuxXxilium/synology-dsm-open-vm-tools" target="_blank">Github / Open VM Tools</a>.</p>
+<p>Nvidia Community Driver Package can be found at <a href="https://github.com/pdbear/syno_nvidia_gpu_driver" target="_blank">Github / Syno Nvidia GPU Driver</a>.</p>
+EOF
+)"
   synodsmnotify -e false -b false "@administrators" "arc_notify_subject" "{\"%NOTIFICATION%\": \"${NOTIFICATION}\", \"%SUBJECT%\": \"${SUBJECT}\"}"
 fi
